1b8b208cf6026766cb31d9edca7aa368148544d4
Cmendivil <CMendivil7@icloud.com>
2017-04-11 21:29:26 -0600

    Script to restart game

---
+++ Fallout.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fallout : MonoBehaviour {
+	// Use this for initialization
+	Vector2 checkpoint;
+	void Start () {
+		checkpoint = new Vector2 (0, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -10) {
+			transform.position = checkpoint;
+		}
+	}
+}
