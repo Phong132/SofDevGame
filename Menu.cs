9e2b24fb835b232d4bd0cb57fa877493cee80b1d
Cmendivil <CMendivil7@icloud.com>
2017-05-03 16:37:58 -0600

    Comments to Menu

--- Menu.cs
+++ Menu.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Script used in Main Menu
 public class Menu : MonoBehaviour
 {
+    
     public int playerLives;
 
     public int startScore;
 
+//Function used by play button and High Score
     public void LoadScene(string name)
     {
         Application.LoadLevel(name);
@@ -17,6 +20,8 @@ public class Menu : MonoBehaviour
         PlayerPrefs.SetInt("CurrentScore", startScore);
     }
 
+//function used by exit button
+//It only works when you build the game
     public void Exit()
     {
         Application.Quit();
