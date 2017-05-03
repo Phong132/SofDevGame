096bd04e70369e4071071c826aafb789042201b8
shadowelecman <shadowelecman@hotmail.com>
2017-05-03 16:34:16 -0600

    Update score.cs

--- Script/score.cs
+++ Script/score.cs
@@ -8,14 +8,14 @@ class gameScores : IComparable<gameScores>
     public int score { get; set; }
     public string name { get; set; }
     public int ID { get; set; }
-
+    //scores have the score, player name, and id which tells when they were added
     public gameScores(int id, int score, string name)
     {
         this.score = score;
         this.name = name;
         this.ID = id;
     }
-
+    //sorts the game scores by score and time when they were added to the database
     public int CompareTo(gameScores other)
     {
         if (other.score < this.score)
