b19a59d249363787891522285ea7aa14ff348fcd
Bravdica <rybr7647@colorado.edu>
2017-05-03 16:43:40 -0600

    commented

--- ScoreManager.cs
+++ ScoreManager.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
 
+    	//created integer score to keep track of score on this script
 	public static int score;
 
 	Text text;
@@ -13,11 +14,11 @@ public class ScoreManager : MonoBehaviour {
 	{
 		text = GetComponent<Text> ();
 
-        //score = 0;
-
+        //score is set to playerprefs which constantly holds the current/updated score
         score = PlayerPrefs.GetInt("CurrentScore");
 	}
 
+    	//updates the score if called upon and displays the score on the screen
 	void Update()
 	{
 		if (score < 0)
@@ -26,12 +27,18 @@ public class ScoreManager : MonoBehaviour {
 	}
 
 	public static void AddPoints (int pointsToAdd){
+        //if a player picks up coins then points are added to their score through this function
 		score += pointsToAdd;
+        
+        //playerprefs is updated to the higher score
         PlayerPrefs.SetInt("CurrentScore", score);
 	}
 
 	public static void Reset(){
+        //if a player dies their score is reset to 0
 		score = 0;
+
+        //playerprefs is updated to the reset score
         PlayerPrefs.SetInt("CurrentScore", score);
     }
 }
